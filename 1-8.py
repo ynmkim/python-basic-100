eaa504329a09344a7320645ec62058a04273a555
김윤미 <younmeet@gmail.com>
2023-08-17 22:30:00 +0900

    1 ~ 8번 문제 해결

---
+++ 1-8.py
@@ -0,0 +1,20 @@
+# 1 ~ 8
+print("Hello")
+
+print("Hello World")
+# 또는
+print("Hello", "World")
+
+print("Hello\nWorld")
+
+print("'Hello'")
+
+print('"Hello World"')
+# 또는
+print("\"Hello World\"")
+
+print("\"!@#$%^&*()'")
+
+print("\"C:\\Download\\\'hello'.py\"") # \도 안전하게 출력하려면 \\를 사용하는 것이 좋다.
+
+print("print(\"Hello\\nWorld\")")
